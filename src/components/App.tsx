c03c175de10c5c7a3c3af1534b7baed44053df51
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-11 23:03:45 -0700

    working on useSelector part in the app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import '../App.css';
+import '../styles/App.css';
 import { useSelector } from "react-redux";
 import { NewTodoInput } from './NewTodoInput';
 import { TodosState } from '../redux/todosReducer';
 
-function App() {
+const App = () => {
   const addTodo = () => {
     // bring state from redux using useSelector hooks
     // useSelector<Type, return type of useSelector>
-    const todos = useSelector<TodosState, TodosState["todos"]>((state) => state.todos)
+    const todos = useSelector<TodosState, TodosState["todos"]>((state) => state.todos);
 
   }
 
@@ -17,7 +17,10 @@ function App() {
       <NewTodoInput addTodo={alert}/>
       <hr/>
       <ul>
-        <li>Some Todo</li>
+        {/* {todos.map((todo) => {
+          <li key={todo}>{todo}</li>
+
+        })} */}
       </ul>
     </>
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
+import {Provider} from "react-redux"
 import { store } from './redux/store';
 
 ReactDOM.render(
