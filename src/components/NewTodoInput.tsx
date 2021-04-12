6775b899c9aa5d02d3e3a31c17ab886dee1169bd
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-11 21:56:43 -0700

    Add onclick event handler function

--- src/components/NewTodoInput.tsx
+++ src/components/NewTodoInput.tsx
@@ -8,12 +8,27 @@ export const NewTodoInput:React.FC<NewTodoInputProps> = ({ addTodo }) => {
   const [todo, setTodo] = React.useState("")
   const [deadline, setDeadline] = React.useState(0)
 
-  const changeHandler:React.ChangeEventHandler<HTMLInputElement> = (event) => {
+  // // way 1
+  // const changeHandler:React.ChangeEventHandler<HTMLInputElement> = (event) => {
+  //   if (event.target.name === 'todo') {
+  //     setTodo(event.target.value)
+  //   } else {
+  //     setDeadline(Number(event.target.value))
+  //   }
+  // }
+
+  // way2
+  const changeHandler= (event:React.ChangeEvent<HTMLInputElement> ) => {
     if (event.target.name === 'todo') {
       setTodo(event.target.value)
     } else {
       setDeadline(Number(event.target.value))
     }
+  }
+
+  // callback for onClick
+  const AddTodoClick= (event: React.MouseEvent) => {
+    event.preventDefault()
 
   }
 
@@ -21,7 +36,7 @@ export const NewTodoInput:React.FC<NewTodoInputProps> = ({ addTodo }) => {
     <div>
       <input type='text' value={todo} name='todo' onChange={changeHandler} placeholder='Write todo...' />
       <input type='number' value={deadline} name='deadline' onChange={changeHandler} placeholder='Deadline...' />
-      <button>Add Todo task</button>
+      <button onClick={AddTodoClick}>Add Todo task</button>
     </div>
   )
 
