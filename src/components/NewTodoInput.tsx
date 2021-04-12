1b4586df0b0609c345d54e599c4a24fe7aebf94d
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-11 21:47:40 -0700

    add onchange handler

--- src/components/NewTodoInput.tsx
+++ src/components/NewTodoInput.tsx
@@ -6,10 +6,16 @@ interface NewTodoInputProps {
 
 export const NewTodoInput:React.FC<NewTodoInputProps> = ({ addTodo }) => {
   const [todo, setTodo] = React.useState("")
+  const [deadline, setDeadline] = React.useState(0)
+
+  const changeHandler:React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setTodo(event.target.value)
+  }
 
   return (
     <div>
-      <input type='text' placeholder='Write todo...' />
+      <input type='text' value={todo} onChange={changeHandler} placeholder='Write todo...' />
+      <input type='number' value={todo} onChange={changeHandler} placeholder='Deadline...' />
       <button>Add Todo task</button>
     </div>
   )
