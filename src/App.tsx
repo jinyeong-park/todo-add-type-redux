e21fd2a1a7b6e7fd56311c23807855898ed199b7
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-11 21:34:47 -0700

    Create todoinput in the component and connect to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
+import { NewTodoInput } from './components/NewTodoInput';
 
 function App() {
   return (
+    <>
+      <NewTodoInput />
+      <hr/>
+      <ul>
+        <li>Some Todo</li>
+      </ul>
+    </>
 
   );
 }
---
+++ src/components/NewTodoInput.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const NewTodoInput = () => {
+  return (
+    <div>
+      <input type='text' placeholder='Write todo...' />
+      <button>Add Todo task</button>
+    </div>
+  )
+
+}
