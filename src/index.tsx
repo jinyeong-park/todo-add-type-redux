03110ce5f936efe91803eaec3596b103ba637a6b
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-11 22:06:40 -0700

    install  @types/react-redux and react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
